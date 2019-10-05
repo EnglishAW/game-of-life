5d2fa928270fff0a8ee3f3f89df695621f6ce312
Andrew English <aenglish@diligent.com>
2019-10-05 10:54:55 -0400

    Use the bootstrap to create app div

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,4 +1,9 @@
 import { h, render } from 'preact'
 import { App } from './app'
 
+let g = document.createElement('div')
+g.setAttribute('id', 'app')
+let body = document.getElementsByTagName('body')
+body[0].appendChild(g)
+
 render(<App />, document.getElementById('app'))
