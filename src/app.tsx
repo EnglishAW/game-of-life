96aefc8d07814b022fb3f5f96c4e2f993d945b10
Andrew English <aenglish@diligent.com>
2019-10-05 11:35:26 -0400

    Added Github Corner Link

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,27 @@
 import { h } from 'preact'
 import GameContainer from './components/GameContainer'
+import { GithubCorner } from './ui-components/GithubCorner'
 import { css } from 'emotion'
 
 export const App = () => {
     return (
-        <div
-            className={css`
-                display: flex;
-                flex-flow: column;
-                justify-content: center;
-                align-items: center;
-            `}
-        >
-            <h1>Game of Life</h1>
-            <GameContainer />
+        <div>
+            <GithubCorner
+                link="https://github.com/EnglishAW/game-of-life"
+                fill="#64CEAA"
+                logoColor="#292d3f"
+            />
+            <div
+                className={css`
+                    display: flex;
+                    flex-flow: column;
+                    justify-content: center;
+                    align-items: center;
+                `}
+            >
+                <h1>Game of Life</h1>
+                <GameContainer />
+            </div>
         </div>
     )
 }
